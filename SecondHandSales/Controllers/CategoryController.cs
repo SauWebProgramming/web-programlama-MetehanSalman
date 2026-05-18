d983925dc0e6ed041f659623cee83057e95e9bb5
Metehan Salman <you@example.com>
2026-05-18 15:35:45 +0300

    Kategori yonetimi controller ve arayuzleri eklendi

---
+++ SecondHandSales/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecondHandSales.Models;
+using SecondHandSales.Repositories;
+
+namespace SecondHandSales.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	public class CategoryController : Controller
+	{
+		private readonly IGenericRepository<Category> _categoryRepo;
+
+		public CategoryController(IGenericRepository<Category> categoryRepo)
+		{
+			_categoryRepo = categoryRepo;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_categoryRepo.GetAll());
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_categoryRepo.Add(category);
+				_categoryRepo.Save();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(category);
+		}
+	}
+}
